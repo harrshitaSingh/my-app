9e697f17b10dbaae98bc797979256f69b5a05f06
Harshita <hsingharshita01@gmail.com>
2024-03-20 21:09:41 +0530

    Image fixed

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,4 +1,3 @@
-import React, { useEffect } from "react";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getMessaging } from "firebase/messaging";
@@ -8,23 +7,23 @@ let firebaseConfig = {};
 
 if (environment === "production") {
   firebaseConfig = {
-    apiKey: "<your-api-key>",
-    authDomain: "<your-auth-domain>",
-    projectId: "<your-project-id>",
-    storageBucket: "<your-storage-bucket>",
-    messagingSenderId: "<your-messaging-sender-id>",
-    appId: "<your-app-id>",
-    measurementId: "<your-measurement-id>",
+    apiKey: "AIzaSyCJVQl2nkZXe36-mhmfG0xQ2PSgaDBP3v8",
+    authDomain: "charming-shield-300804.firebaseapp.com",
+    projectId: "charming-shield-300804",
+    storageBucket: "charming-shield-300804.appspot.com",
+    messagingSenderId: "303239150168",
+    appId: "1:303239150168:web:e5a1bd2adbcdc752cae82d",
+    measurementId: "G-68LY2278VG",
   };
 } else {
   firebaseConfig = {
-    apiKey: "<your-api-key>",
-    authDomain: "<your-auth-domain>",
-    projectId: "<your-project-id>",
-    storageBucket: "<your-storage-bucket>",
-    messagingSenderId: "<your-messaging-sender-id>",
-    appId: "<your-app-id>",
-    measurementId: "<your-measurement-id>",
+    apiKey: "AIzaSyDh1xGKmWaRStY2z_XLCMgrLs6Onvj2xYI",
+    authDomain: "startstaging.firebaseapp.com",
+    projectId: "startstaging",
+    storageBucket: "startstaging.appspot.com",
+    messagingSenderId: "546936271294",
+    appId: "1:546936271294:web:cf223dde266eb0354e89ef",
+    measurementId: "G-NYB7DCYEPY",
   };
 }
 
@@ -34,4 +33,4 @@ const analytics = getAnalytics(firebaseApp);
 const messaging = getMessaging(firebaseApp);
 
 export default firebaseApp;
-export { analytics, messaging };
+export  { analytics, messaging };
